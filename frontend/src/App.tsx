2573f3e23d209f16371120e130f29af0d3c838dc
ar-ease <arghya0017@gmail.com>
2024-11-10 17:12:47 +0530

    client inits

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>hii there</h1>
+      <h2>this is h2 tag</h2>
+      <div className="bg-red-500">hello</div>
+    </>
+  );
+}
+
+export default App;
